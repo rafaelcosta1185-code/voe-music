58522c0d92ebbd41426febbfbb770d14b1e414a0
rafaelcosta1185-code <rafaelcosta1185@yahoo.com.br>
2026-08-07 18:47:55 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='voe-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html'])).then(()=>self.skipWaiting())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))))
