7cc14371cad49c3783a498d2633b8d4e71f411af
rafaelcosta1185-code <rafaelcosta1185@yahoo.com.br>
2026-08-07 18:49:21 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='voe-v13';const A=['/','/index.html','/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
